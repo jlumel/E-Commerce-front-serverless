2dc0486c4328d490b2d4d6d62ffaff9c55b5dfdb
Lumel <jlumelsky@gmail.com>
2020-10-21 12:34:52 -0300

    Segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import Img from './components/img'
-import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Img/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar/>
       </header>
     </div>
   );
