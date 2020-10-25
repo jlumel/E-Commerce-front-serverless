997cabfd12e6650c00d9dd83731a88772e4a6665
Lumel <jlumelsky@gmail.com>
2020-10-25 15:40:48 -0300

    Ordene styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ItemListContainer from './ItemListContainer'
 
-const Home = () => {
-    
+const Home = ({ title }) => {
+    let hora = new Date()
+
+    if (hora.getHours() > 6 && hora.getHours() < 12) {
+        title = 'Buen dia!'
+    } else if (hora.getHours() > 12 && hora.getHours() < 20) {
+        title = 'Buenas tardes'
+    } else {
+        title = 'Buenas noches'
+    }
     return (
-            <ItemListContainer />
+        <main>
+            <ItemListContainer title={title} />
+        </main>
+
     )
 }
 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,15 +1,7 @@
 import React from 'react'
 
 const ItemListContainer = ({title})=> {
-    let hora = new Date()
-
-    if (hora.getHours() > 6 && hora.getHours() < 12) {
-        title = 'Buen dia!'
-    } else if (hora.getHours() > 12 && hora.getHours() < 20) {
-        title = 'Buenas tardes'
-    } else {
-        title = 'Buenas noches'
-    }
+    
 return (
     
 <h2 style={{ fontFamily: 'Roboto' , marginLeft: '0.5vw'}}>{title}</h2>
